21946a92716a415755b00bf22f85800c525c5992
siranjeevi <siranjeeviaug4@gmail.com>
2026-03-26 12:56:49 +0530

    Added uc-1 code

---
+++ Hello_App/src/main/java/org/example/UC1_Hello_App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class UC1_Hello_App {
+
+        public static void main(String[] args) {
+
+            // Display message
+            System.out.println("Hello World");
+
+        }
+    }
---
+++ Hello_App/uc1.java
@@ -0,0 +1,9 @@
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        // Display message
+        System.out.println("Hello World");
+
+    }
+}
