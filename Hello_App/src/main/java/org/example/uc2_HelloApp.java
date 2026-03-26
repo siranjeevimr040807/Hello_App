15c12c2f596d07340dd89709fa2ae12231c893c7
siranjeevi <siranjeeviaug4@gmail.com>
2026-03-26 13:00:49 +0530

    Added uc-2 code

---
+++ Hello_App/src/main/java/org/example/uc2_HelloApp.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class uc2_HelloApp {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
+    }
+}
+
