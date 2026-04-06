8b4e8473fe31af8370549c74dd347a6c05fd4ddf
siranjeevimr040807 <siranjeevi040807@gmail.com>
2026-04-06 10:52:28 +0530

    Delete Hello_App/src/main/java/org/example/uc3.java

--- Hello_App/src/main/java/org/example/uc3.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class uc3 public class HelloApp {
-    public static void main(String[] args) {
-        String name = args.length > 0 ? args[0] : "World";
-        System.out.println("Hello, " + name + "!");
-    }
-{
-}
